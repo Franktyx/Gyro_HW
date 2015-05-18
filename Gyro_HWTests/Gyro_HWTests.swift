1cf750f0ca4599689b7e62a3bb9956b246698cbe
Bingqing Xia <Summer@Bingqings-MacBook-Pro.local>
2015-05-18 15:24:35 -0500

    Initial Commit

---
+++ Gyro_HWTests/Gyro_HWTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gyro_HWTests.swift
+//  Gyro_HWTests
+//
+//  Created by Bingqing Xia on 5/18/15.
+//  Copyright (c) 2015 TYX. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Gyro_HWTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
